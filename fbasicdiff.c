21b4aa54bde3488b63750fa06a04d761d77e5d01
nileshk98 <nileshjvk1998@gmail.com>
2017-10-10 09:51:21 +0530

    Basic Diff

--- fbasicdiff.c
+++ fbasicdiff.c
@@ -8,12 +8,25 @@
 #include<errno.h>
 #include "diff.h"
 
-int main(int argc, char *argv[]) {
-	int i = 0, j = 0, m, n, d;
+int main(int argc, char * const argv[]) {
+	int i = 0, j = 0, m, n, d, count = 0;
 	file1 f1;
 	file2 f2;
-	f1.filename = argv[1];
-	f2.filename = argv[2];
+	const char *optlist;
+	char c;
+	optlist = "rcytiw";
+
+	bool ignore_space_change = false;
+	bool recursive_dir_diff = false;
+	bool context_format = false;
+	bool side_by_side = false;
+	bool normal_diff = true;
+	bool expand_tabs = false;
+	bool ignore_case = false;
+	bool ignore_all_space = false;
+
+	f1.filename = argv[argc - 2];
+	f2.filename = argv[argc - 1];
 	f1.fd = open(f1.filename, O_RDONLY);
 	if(f1.fd == -1) {
 		printf("mydiff: %s: No such file or directory\n", f1.filename);
@@ -31,6 +44,31 @@ int main(int argc, char *argv[]) {
 	}
 	for(i = 0; i < f2.nol; i++) {
 		//printf("%s\n", f2.lines[i]);
+	}
+	while((c = getopt(argc, argv, optlist)) != -1) {
+		switch(c) {
+			case 'r' : recursive_dir_diff = true;
+					   break;
+			case 'c' : context_format = true;
+					   break;
+			case 'y' : if(context_format != true) { 
+					   		side_by_side = true;
+						   count++;
+					   }
+					   else {
+					   }
+					   break;
+			case 't' : expand_tabs = true;
+					   break;
+			case 'i' : ignore_case = true;
+					   ignorecase(&f1, &f2);	
+					   break;
+			case 'w' : ignore_all_space = true;
+					   ignorespace(&f1, &f2);
+					   break;
+			case '?' : break;
+		}
+				   
 	}
 	d = shortestpath(&f1, &f2);
 	//printf("Shortest number of edits %d\n", d);
