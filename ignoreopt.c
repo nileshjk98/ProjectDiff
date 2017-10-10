f7e06405f47767770e6fc6a025e7d566b908047c
nileshk98 <nileshjvk1998@gmail.com>
2017-10-10 09:54:04 +0530

    Ignore Options

---
+++ ignoreopt.c
@@ -0,0 +1,91 @@
+#include<string.h>
+#include<stdlib.h>
+#include "diff.h"
+void strlwr(char *str) {
+	int i;
+	for(i = 0; str[i] != '\0'; i++) {
+		if(str[i] >= 'A' && str[i] <= 'Z') {
+			str[i] = str[i] + ('a' - 'A');
+		}
+	}
+}
+void ignorespace(file1 *f1, file2 *f2) {
+	char str[1024];
+	int i, j, k = 0;
+	for(i = 0; i < f1->nol; i++) {
+		for(j = 0; f1->lines[i][j] != '\0'; j++) {
+			if(f1->lines[i][j] != ' ' && f1->lines[i][j] != '\t') {
+				str[k++] = f1->lines[i][j];
+			}
+		}		
+		str[k] = '\0';
+		f1->lines[i] = (char *)malloc(strlen(str) + 1);
+		strcpy(f1->lines[i], str);
+		k = 0;
+	}
+	for(i = 0; i < f2->nol; i++) {
+		for(j = 0; f2->lines[i][j] != '\0'; j++) {
+			if(f2->lines[i][j] != ' ' && f2->lines[i][j] != '\t') {
+				str[k++] = f2->lines[i][j];
+			}
+		}		
+		str[k] = '\0';
+		f2->lines[i] = (char *)malloc(strlen(str) + 1);
+		strcpy(f2->lines[i], str);
+		k = 0;
+	}
+}
+void ignorespchange(file1 *f1, file2 *f2) {
+	int i, j, k = 0;
+	char str[1024], prev;
+	for(i = 0; i < f1->nol; i++) {
+		if(f1->lines[i][strlen(f1->lines[i]) - 1] == ' ') {
+			j = strlen(f1->lines[i]) - 1;
+			while(f1->lines[i][j] == ' ') {
+				j--;
+			}
+			f1->lines[i][j + 1] = '\0';
+		}
+		prev = f1->lines[i][0];
+		for(j = 0; f1->lines[i][j] != '\0'; j++) {
+			if(prev != ' ') {
+				str[k++] = f1->lines[i][j];
+				prev = f1->lines[i][j];
+			}
+		}
+		str[k] = '\0';
+		k = 0;
+		f1->lines[i] = (char *)malloc(strlen(str) + 1);
+		strcpy(f1->lines[i], str);
+	}
+	for(i = 0; i < f2->nol; i++) {
+		if(f2->lines[i][strlen(f2->lines[i]) - 1] == ' ') {
+			j = strlen(f2->lines[i]) - 1;
+			while(f2->lines[i][j] == ' ') {
+				j--;
+			}
+			f2->lines[i][j + 1] = '\0';
+		}
+		prev = f2->lines[i][0];
+		for(j = 0; f2->lines[i][j] != '\0'; j++) {
+			if(prev != ' ') {
+				str[k++] = f2->lines[i][j];
+				prev = f2->lines[i][j];
+			}
+		}
+		str[k] = '\0';
+		k = 0;
+		f2->lines[i] = (char *)malloc(strlen(str) + 1);
+		strcpy(f2->lines[i], str);
+	}
+
+}
+void ignorecase(file1 *f1, file2* f2) {
+	int i;
+	for(i = 0; i < f1->nol; i++) {
+		strlwr(f1->lines[i]);		
+	}
+	for(i = 0; i < f2->nol; i++) {
+		strlwr(f2->lines[i]);		
+	}
+}
