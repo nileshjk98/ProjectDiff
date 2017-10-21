501f0b4bd3710aa1823df23b9a5a14e5a609f8ff
nileshk98 <nileshjvk1998@gmail.com>
2017-10-21 12:26:13 +0530

    contextformat

---
+++ context.c
@@ -0,0 +1,41 @@
+#include<time.h>
+#include<sys/stat.h>
+#include<stdio.h>
+#include "diff.h"
+
+/* 
+ * Shows output in context format
+ *
+ */
+
+void contextformat(file1 *f1, file2 *f2) {
+	int i = 0, j = 0, count = 0, k = 0, flag = 0, l = 0;
+	struct stat attrib;
+	stat(f1->filename, &attrib);
+	char time[100];
+	strftime(time, 100, "%Y-%m-%d %H:%M:%S", localtime(&attrib.st_mtime));
+	printf("*** %s %s\n", f1->filename, time);
+	stat(f2->filename, &attrib);
+	strftime(time, 100, "%Y-%m-%d %H:%M:%S", localtime(&attrib.st_mtime));
+	printf("***************\n");
+	printf("*** %d,%d ****\n", i + 1, f1->nol);
+	for(i = 0 ; i < f1->nol; i++) {
+		if(f1->dellines[i] == 0) {
+			printf("  %s\n", f1->lines[i]);
+		}
+		else {
+			printf("- %s\n", f1->lines[i]);
+		}
+	}
+	i = 0;
+	printf("--- %d,%d ----\n", i + 1, f2->nol);
+	for(i = 0 ; i < f2->nol; i++) {
+		if(f2->toinslines[i] == 0) {
+			printf("  %s\n", f2->lines[i]);
+		}
+		else {
+			printf("+ %s\n", f2->lines[i]);
+		}
+	}
+
+}
